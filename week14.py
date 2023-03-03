0b9177811f6ddfdb3cd8a29be0ba79eeac7e7574
Yuanzhuo2021 <fg20441@bristol.ac.uk>
2023-03-04 00:04:04 +0800

    solve ode using euler method and plot error agaisnt stepsize

--- week14.py
+++ week14.py
@@ -7,25 +7,42 @@ This is a temporary script file.
 
 #use euler's method to solve a ode probelm
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import math
 
 
 
 def euler_step(x0,y0,deltat):
-    y = y0+deltat*x0
-    return y
+    y1 = y0+deltat*x0
+    return y1
 
 
+#the solution to the ode equation
 
-Y = exp(x)
-x0 =1
-deltat = 0.1
-num_steps = 1/deltat
-while 1 <= 1/deltat:
-    y = euler_step(x0,deltat)
-    x0 = x+deltat
-    error = Y - y
+#initial values
+
+error = []
+
+#step size
+deltat = [1,0.5,0.2,0.1,0.05,0.02,0.01,0.005,0.002,0.001]
+
+for t in deltat:
+    x0 =0
+    y0 =1
+    num_steps = 1/t
+    i =1
+    while i <= num_steps:
+        y0 = euler_step(x0,y0,t)
+        x0 = x0+t
+        i += 1
     
+    y_true = math.exp(1)
+    error.append(abs(y_true - y0))
 
-print(y)
+print(y0)
+print(error)
+plt.plot(deltat,error)
+plt.yscale('log')
+plt.xscale('log')
+plt.xlabel('stepsize')
+plt.ylabel('Error')
