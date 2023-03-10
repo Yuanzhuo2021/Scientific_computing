a231b60c67c03d188a5d862dbd23bb4e5564f601
Yuanzhuo2021 <fg20441@bristol.ac.uk>
2023-03-10 20:07:06 +0800

    week14 assignment problem part1 completed

--- week14.py
+++ week14.py
@@ -12,9 +12,9 @@ import math
 
 
 
-def euler_step(x0,y0,deltat):
-    y1 = y0+deltat*x0
-    return y1
+def euler_step(x0,t0,deltat):
+    x1 = x0+deltat*t0
+    return x1
 
 
 #the solution to the ode equation
@@ -26,41 +26,43 @@ error = []
 #step size
 deltat = [1,0.5,0.2,0.1,0.05,0.02,0.01,0.005,0.002,0.001]
 
-for t in deltat:
-    x0 =0
-    y0 =1
-    num_steps = 1/t
+for ele in deltat:
+    t0 =0
+    x0 =1
+    num_steps = 1/ele
     i =1
     while i <= num_steps:
-        y0 = euler_step(x0,y0,t)
-        x0 = x0+t
+        x0 = euler_step(x0,t0,ele)
+        t0 = t0+ele
         i += 1
     
-    y_true = math.exp(1)
-    error.append(abs(y_true - y0))
+    x_true = math.exp(1)
+    error.append(abs(x_true - x0))
 
-print(y0)
+print(x0)
 print(error)
 plt.plot(deltat,error)
 plt.yscale('log')
 plt.xscale('log')
 plt.xlabel('stepsize')
 plt.ylabel('Error')
+plt.title('Error of Eulers method against stepsize deltat')
 
 
 
-def solve_to(x0,t0,t1,deltat_max):
-    deltat = t1-t0
+def solve_to(x1,t1,t2,deltat_max):
+    deltat = t2-t1
     while deltat >= deltat_max:
         deltat = deltat/10
-    return deltat
      
-    num_steps = (t1-t0)/deltat
+    num_steps = (t2-t1)/deltat
     i =1
     while i <= num_steps:
-        x1 = euler_step(t0,x0,deltat)
-        t0 = t0+deltat
+        x1 = euler_step(x1,t1,deltat)
+        t1 = t1+deltat
         i += 1
     return x1
+   
 
-solve_to(1,0,1,0.1)
+x2 = solve_to(1,0,1,0.01)
+print(x2)
