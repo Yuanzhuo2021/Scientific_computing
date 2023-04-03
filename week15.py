66ed26ad4eb095f8f6c53606720fd90545111817
Yuanzhuo2021 <fg20441@bristol.ac.uk>
2023-04-04 05:10:04 +0800

    simulate predator-prey and plot graphs

--- week15.py
+++ week15.py
@@ -4,9 +4,9 @@ Created on Mon Apr  3 18:39:32 2023
 
 @author: YHU
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def predator_prey(x0,y0,a,b,d,deltat):
     dxdt = x0*(1-x0)-(a*x0*y0)/(d+x0)
@@ -22,27 +22,35 @@ def predator_prey_plot(b):
     d = 0.1 
     deltat = 0.01
     x0,y0 = 0.05,0.05
-    solution_x = [x0]
-    solution_y = [y0]
+    soln_x = [x0]
+    soln_y = [y0]
     t = np.linspace(0,200,int(200/deltat)+1)
     for i in range(int(200/deltat)):
         x0,y0 = predator_prey(x0,y0,a,b,d,deltat)
-        solution_x.append(x0)
-        solution_y.append(y0)
+        soln_x.append(x0)
+        soln_y.append(y0)
 
     plt.figure()
-    plt.plot(t,solution_x)
-    plt.plot(t,solution_y)
+    plt.plot(t,soln_x)
+    plt.plot(t,soln_y)
     plt.legend(['Prey','Predator'])
     plt.yticks(np.arange(0,1,0.1))
     plt.xlabel('Time')
     plt.ylabel('Population')
-    return plt
+    plt.show()
+    return soln_x, soln_y
 
 
+soln4_x,soln4_y = predator_prey_plot(0.4)
+soln3_x,soln3_y = predator_prey_plot(0.3)
+soln2_x,soln2_y = predator_prey_plot(0.2)
+soln1_x,soln1_y = predator_prey_plot(0.1)
 
 
-predator_prey_plot(0.4)
-predator_prey_plot(0.2)
-
-
+plt.plot(soln1_x,soln1_y,label = 'b = 0.1')
+plt.plot(soln2_x,soln2_y,label = 'b = 0.2')
+plt.plot(soln3_x,soln3_y,label = 'b = 0.3')
+plt.plot(soln4_x,soln4_y,label = 'b = 0.4')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
