62b772cc8f5ff5b7885239c9b6bbd3a6438eb285
Yuanzhuo2021 <fg20441@bristol.ac.uk>
2023-04-11 22:33:27 +0800

    improvements

--- testscript.py
+++ testscript.py
@@ -5,3 +5,10 @@ Created on Tue Apr 11 19:59:13 2023
 @author: YHU
 """
 
+import unittest
+
+class Test_ODE(unittest.TestCase):
+    def test_soln(self):
+        self.assertAlmostEquals(1,1)
+        
+unittest.main(argv=[''],verbosity=0, exit=False)
