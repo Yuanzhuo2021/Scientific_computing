cca4d1d5e7755624fb12cb5e535b343eda0b3fb8
Yuanzhuo2021 <fg20441@bristol.ac.uk>
2023-04-13 22:37:57 +0800

    finite difference

---
+++ week19.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 21:59:11 2023
+
+@author: YHU
+"""
+
+## finite differenting
---
+++ week22.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 21:59:55 2023
+
+@author: YHU
+"""
+
