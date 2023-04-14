6f0318b409f0d9beb1188a5d0c560db4f38028e6
Yuanzhuo2021 <fg20441@bristol.ac.uk>
2023-04-15 01:07:39 +0800

    natural parameter continuation

--- week17.py
+++ week17.py
@@ -6,29 +6,39 @@ Created on Thu Apr 13 04:51:56 2023
 """
 
 import numpy as np
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve,root
 import matplotlib.pyplot as plt
 
 def algebraic_cubic(x,c):
-    return x**3 - x + c
+    return x**2 - x + c
 
 def continuation(func,rang):
     S = 1000
     step = (rang[1]-rang[0])/S
     # first generate the first value of solution
-    result = fsolve(lambda u: func(u,rang[0]),2)
+    result= root(lambda u: func(u,rang[0]),-2)
     print(rang[0])
-    print(result)
-    solution = result
+    print(result.x)
+    solution = result.x
     C = [rang[0]]
     for i in range(S):
-        c = rang[0] + step*(i)
-        C.append(c)
-        result = fsolve(lambda u: func(u,c),result)
-        solution = np.hstack((solution,result))
-    return np.array([C,solution])
+        c = rang[0] + step*(i+1)
+        result = root(lambda u: func(u,c),result.x)
+        if result.success:
+            result = result
+            C.append(c)
+            solution = np.hstack((solution,result.x))
+        else:
+            print(result.message)
+            print('Converge fails at c = ',c)
+            break
+        
+    return [C,solution]
 
 z = continuation(algebraic_cubic,(-2,2))
 
 
+
 plt.plot(z[0],z[1])
+plt.xlabel('Parameter')
+plt.ylabel('Solution')
